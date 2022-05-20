486f0b045b00ed10af3cc9406685741b66971501
Chathu Vishwajith <4evercreations@gmail.com>
2022-05-20 14:04:05 +0530

    feat: :sparkles: add protected query

--- src/graphql/resolvers/hello.ts
+++ src/graphql/resolvers/hello.ts
@@ -1,9 +1,15 @@
-import { Query, Resolver } from 'type-graphql';
+import { Authorized, Query, Resolver } from 'type-graphql';
 
 @Resolver()
 export class HelloResolver {
   @Query(() => String)
   hello() {
+    return 'Hello';
+  }
+
+  @Authorized()
+  @Query(() => String)
+  helloProtected() {
     return 'Hello';
   }
 }
